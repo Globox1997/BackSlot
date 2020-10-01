e8e35c8e85afbffce5d377651a92c674a307713c
Globox1997 <mitschke11@web.de>
2020-10-01 20:17:11 +0200

    fixed creative inv bug

---
+++ src/main/java/net/backslot/mixin/CreativeInventoryScreenMixin.java
@@ -0,0 +1,35 @@
+package net.backslot.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.text.LiteralText;
+
+@Environment(EnvType.CLIENT)
+@Mixin(CreativeInventoryScreen.class)
+public abstract class CreativeInventoryScreenMixin
+    extends AbstractInventoryScreen<CreativeInventoryScreen.CreativeScreenHandler> {
+
+  public CreativeInventoryScreenMixin(PlayerEntity player) {
+    super(new CreativeInventoryScreen.CreativeScreenHandler(player), player.inventory, LiteralText.EMPTY);
+    player.currentScreenHandler = this.handler;
+  }
+
+  @Inject(method = "setSelectedTab", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/ingame/CreativeInventoryScreen;deleteItemSlot:Lnet/minecraft/screen/slot/Slot;", shift = At.Shift.BEFORE))
+  private void setSelectedTabMixin(ItemGroup group, CallbackInfo info) {
+    for (int i = 0; i < this.handler.slots.size(); ++i) {
+      if (i == 46) {
+        ((CreativeInventoryScreen.CreativeScreenHandler) this.handler).slots.remove(i);
+      }
+    }
+  }
+
+}
