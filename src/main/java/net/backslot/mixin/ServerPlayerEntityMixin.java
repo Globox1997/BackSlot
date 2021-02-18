1b32865cccf7a00530385553ec6015611a8c1001
Globox1997 <mitschke11@web.de>
2021-02-18 21:17:17 +0100

    added partly gravestone compat

--- src/main/java/net/backslot/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/backslot/mixin/ServerPlayerEntityMixin.java
@@ -16,9 +16,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import net.backslot.network.SyncPacket;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -46,6 +49,22 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
         sendPacket(42);
       }
       beltSlotStack = this.inventory.getStack(42);
+    }
+  }
+
+  // Gravestone compat
+  @Inject(method = "onDeath", at = @At("HEAD"))
+  public void onDeathMixin(DamageSource source, CallbackInfo info) {
+    if (FabricLoader.getInstance().isModLoaded("gravestones")
+        && !this.world.getGameRules().getBoolean(GameRules.KEEP_INVENTORY)) {
+      if (!this.inventory.getStack(41).isEmpty()) {
+        this.dropStack(this.inventory.getStack(41));
+        this.inventory.removeStack(41);
+      }
+      if (!this.inventory.getStack(42).isEmpty()) {
+        this.dropStack(this.inventory.getStack(42));
+        this.inventory.removeStack(42);
+      }
     }
   }
 
