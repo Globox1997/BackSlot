6a74944d878ae6830e87b88993fc2a76d21cbe03
Globox1997 <mitschke11@web.de>
2021-07-22 19:19:41 +0200

    added annotations

--- src/main/java/net/backslot/config/BackSlotConfig.java
+++ src/main/java/net/backslot/config/BackSlotConfig.java
@@ -9,19 +9,24 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 @Config.Gui.Background("minecraft:textures/block/stone.png")
 public class BackSlotConfig implements ConfigData {
     public boolean backslot_sounds = true;
-    @Comment("Use for mod compatibility - Reload world necessary")
+    @ConfigEntry.Gui.RequiresRestart
+    @Comment("Use for mod compatibility")
     public boolean change_slot_arrangement = false;
     public boolean disable_backslot_hud = false;
     public boolean switch_beltslot_side = false;
     @ConfigEntry.Category("advanced_settings")
+    @ConfigEntry.Gui.RequiresRestart
     @ConfigEntry.Gui.PrefixText
     public int backSlot_x = 0;
     @ConfigEntry.Category("advanced_settings")
+    @ConfigEntry.Gui.RequiresRestart
     @Comment("Example: -18 on Y to move up one slot")
     public int backSlot_y = 0;
     @ConfigEntry.Category("advanced_settings")
+    @ConfigEntry.Gui.RequiresRestart
     public int beltSlot_x = 0;
     @ConfigEntry.Category("advanced_settings")
+    @ConfigEntry.Gui.RequiresRestart
     public int beltSlot_y = 0;
     @ConfigEntry.Category("advanced_settings")
     public float backslot_scale = 2.0F;
