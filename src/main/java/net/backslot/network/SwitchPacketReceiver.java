fec26b685f7a9bef92efc12bb66f3ad814a969e4
Globox1997 <mitschke11@web.de>
2023-08-16 09:40:36 +0200

    fixed unsafe handling

--- src/main/java/net/backslot/network/SwitchPacketReceiver.java
+++ src/main/java/net/backslot/network/SwitchPacketReceiver.java
@@ -43,134 +43,136 @@ public class SwitchPacketReceiver implements ServerPlayNetworking.PlayChannelHan
         // target slot
         int slot = buffer.readInt();
 
-        // player inventory and selected slot
-        PlayerInventory playerInventory = player.getInventory();
-        int selectedSlot = playerInventory.selectedSlot;
+        server.execute(() -> {
+            // player inventory and selected slot
+            PlayerInventory playerInventory = player.getInventory();
+            int selectedSlot = playerInventory.selectedSlot;
 
-        // think in a way of pulling out case (although it can be just a putting back)
-        int slotToPullOutFrom = slot;
-        int slotToPullOutTo = selectedSlot;
+            // think in a way of pulling out case (although it can be just a putting back)
+            int slotToPullOutFrom = slot;
+            int slotToPullOutTo = selectedSlot;
 
-        // pull out to offhand if pulling out from belt slot and offhand switch is on,
-        // aka belt slot always goes with offhand
-        if (BackSlotMain.CONFIG.offhand_switch) {
-            if (slotToPullOutTo != 40) {
-                if (slotToPullOutFrom == 42) {
-                    slotToPullOutTo = 40;
-                }
-            }
-        }
-
-        // shield in the back would always be pulled out to offhand
-        if (BackSlotMain.CONFIG.offhand_shield) {
-            if (slotToPullOutTo != 40) {
-                if (slotToPullOutFrom == 41) {
-                    ItemStack backSlotStack = playerInventory.getStack(slotToPullOutFrom);
-                    boolean shouldPullOutToOffHand = backSlotStack.getItem() instanceof ShieldItem;
-                    if (shouldPullOutToOffHand) {
+            // pull out to offhand if pulling out from belt slot and offhand switch is on,
+            // aka belt slot always goes with offhand
+            if (BackSlotMain.CONFIG.offhand_switch) {
+                if (slotToPullOutTo != 40) {
+                    if (slotToPullOutFrom == 42) {
                         slotToPullOutTo = 40;
                     }
                 }
             }
-        }
 
-        // fallback early to offhand if failure in main hand is expected
-        if (BackSlotMain.CONFIG.offhand_fallback) {
-            if (slotToPullOutTo != 40) {
-                ItemStack slotStack = playerInventory.getStack(slotToPullOutFrom);
-                if (slotStack.isEmpty()) {
-                    ItemStack mainHandStack = playerInventory.getStack(slotToPullOutTo);
-                    boolean nothingsGonnaHappenWithTheMainHand = mainHandStack.isEmpty() || !isItemAllowed(mainHandStack, slotToPullOutFrom);
-                    if (nothingsGonnaHappenWithTheMainHand) {
-                        ItemStack offHandStack = playerInventory.offHand.get(0);
-                        if (isItemAllowed(offHandStack, slotToPullOutFrom)) {
+            // shield in the back would always be pulled out to offhand
+            if (BackSlotMain.CONFIG.offhand_shield) {
+                if (slotToPullOutTo != 40) {
+                    if (slotToPullOutFrom == 41) {
+                        ItemStack backSlotStack = playerInventory.getStack(slotToPullOutFrom);
+                        boolean shouldPullOutToOffHand = backSlotStack.getItem() instanceof ShieldItem;
+                        if (shouldPullOutToOffHand) {
                             slotToPullOutTo = 40;
                         }
                     }
                 }
             }
-        }
 
-        // check items in both slots
-        ItemStack stackInSlotToPullOutFrom = playerInventory.getStack(slotToPullOutFrom);
-        ItemStack stackInSlotToPullOutTo = playerInventory.getStack(slotToPullOutTo);
-
-        // switching is meaningless if both slots are empty
-        boolean bothSlotsAreEmpty = stackInSlotToPullOutFrom.isEmpty() && stackInSlotToPullOutTo.isEmpty();
-        if (bothSlotsAreEmpty) {
-            return;
-        }
-
-        // slot and stack information for putting back
-        int slotToPutBackTo = slotToPullOutFrom;
-        ItemStack stackToPutBack = stackInSlotToPullOutTo;
-
-        // check conditions
-        boolean doneSwitching = false;
-        boolean canPutBack = isItemAllowed(stackToPutBack, slotToPutBackTo);
-        if (canPutBack) {
-            // just do switch
-            playerInventory.setStack(slotToPullOutTo, stackInSlotToPullOutFrom); // pull out
-            playerInventory.setStack(slotToPutBackTo, stackToPutBack); // put back
-            playerInventory.markDirty();
-            doneSwitching = true;
-        } else if (BackSlotMain.CONFIG.put_aside) {
-            // try to put aside to an empty inventory slot if there is any
-            int slotToPutAside = playerInventory.getEmptySlot();
-            boolean canPutAside = slotToPutAside >= 0;
-            if (canPutAside) {
-                // do switch while putting item being held aside
-                ItemStack stackToPutAside = stackToPutBack;
-                stackToPutBack = ItemStack.EMPTY;
-                playerInventory.setStack(slotToPutAside, stackToPutAside); // put aside
-                playerInventory.setStack(slotToPullOutTo, stackInSlotToPullOutFrom); // pull out
-                playerInventory.setStack(slotToPutBackTo, stackToPutBack); // put back
-                playerInventory.markDirty();
-                doneSwitching = true;
-            } else if (BackSlotMain.CONFIG.drop_holding) {
-                // even drop an item stack being held in order to make an empty space for
-                // pulling out
-                ItemStack stackToDrop = playerInventory.removeStack(slotToPullOutTo);
-                ItemStack stackRemaining = playerInventory.getStack(slotToPullOutTo);
-                // line below is from implementation of ServerPlayerEntity.dropSelectedItem()
-                // without fully understanding what it does
-                player.currentScreenHandler.getSlotIndex(playerInventory, slotToPullOutTo).ifPresent((i) -> {
-                    player.currentScreenHandler.setPreviousTrackedSlot(i, stackRemaining);
-                });
-                player.dropItem(stackToDrop, false, true);
-                stackToPutBack = ItemStack.EMPTY;
-                // do switch with an empty hand
-                playerInventory.setStack(slotToPullOutTo, stackInSlotToPullOutFrom); // pull out
-                playerInventory.setStack(slotToPutBackTo, stackToPutBack); // put back
-                playerInventory.markDirty();
-                doneSwitching = true;
-            }
-        }
-
-        // play sound if done switching
-        if (doneSwitching) {
-            if (BackSlotMain.CONFIG.backslot_sounds) {
-                if (stackInSlotToPullOutTo.isEmpty() && !stackInSlotToPullOutFrom.isEmpty()) {
-                    if (stackInSlotToPullOutFrom.getItem() instanceof SwordItem) {
-                        // pulling out sword to an empty hand
-                        player.getWorld().playSound(null, player.getBlockPos(), BackSlotSounds.SHEATH_SWORD_EVENT, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                    } else {
-                        // pulling out others to an empty hand
-                        player.getWorld().playSound(null, player.getBlockPos(), SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, SoundCategory.PLAYERS, 1.0F, 1.0F);
+            // fallback early to offhand if failure in main hand is expected
+            if (BackSlotMain.CONFIG.offhand_fallback) {
+                if (slotToPullOutTo != 40) {
+                    ItemStack slotStack = playerInventory.getStack(slotToPullOutFrom);
+                    if (slotStack.isEmpty()) {
+                        ItemStack mainHandStack = playerInventory.getStack(slotToPullOutTo);
+                        boolean nothingsGonnaHappenWithTheMainHand = mainHandStack.isEmpty() || !isItemAllowed(mainHandStack, slotToPullOutFrom);
+                        if (nothingsGonnaHappenWithTheMainHand) {
+                            ItemStack offHandStack = playerInventory.offHand.get(0);
+                            if (isItemAllowed(offHandStack, slotToPullOutFrom)) {
+                                slotToPullOutTo = 40;
+                            }
+                        }
                     }
-                } else if (stackInSlotToPullOutFrom.getItem() instanceof SwordItem) {
-                    // pulling out sword to a non empty hand
-                    player.getWorld().playSound(null, player.getBlockPos(), BackSlotSounds.SHEATH_SWORD_EVENT, SoundCategory.PLAYERS, 1.0F, 0.9F + player.getRandom().nextFloat() * 0.2F);
-                } else if (stackInSlotToPullOutTo.getItem() instanceof SwordItem) {
-                    // putting back sword item (including while pulling out what there was other
-                    // than sword)
-                    player.getWorld().playSound(null, player.getBlockPos(), BackSlotSounds.PACK_UP_ITEM_EVENT, SoundCategory.PLAYERS, 1.0F, 0.9F + player.getRandom().nextFloat() * 0.2F);
-                } else if (!stackInSlotToPullOutTo.isEmpty()) {
-                    // putting back other than sword
-                    player.getWorld().playSound(null, player.getBlockPos(), SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, SoundCategory.PLAYERS, 1.0F, 1.0F);
                 }
             }
-        }
+
+            // check items in both slots
+            ItemStack stackInSlotToPullOutFrom = playerInventory.getStack(slotToPullOutFrom);
+            ItemStack stackInSlotToPullOutTo = playerInventory.getStack(slotToPullOutTo);
+
+            // switching is meaningless if both slots are empty
+            boolean bothSlotsAreEmpty = stackInSlotToPullOutFrom.isEmpty() && stackInSlotToPullOutTo.isEmpty();
+            if (bothSlotsAreEmpty) {
+                return;
+            }
+
+            // slot and stack information for putting back
+            int slotToPutBackTo = slotToPullOutFrom;
+            ItemStack stackToPutBack = stackInSlotToPullOutTo;
+
+            // check conditions
+            boolean doneSwitching = false;
+            boolean canPutBack = isItemAllowed(stackToPutBack, slotToPutBackTo);
+            if (canPutBack) {
+                // just do switch
+                playerInventory.setStack(slotToPullOutTo, stackInSlotToPullOutFrom); // pull out
+                playerInventory.setStack(slotToPutBackTo, stackToPutBack); // put back
+                playerInventory.markDirty();
+                doneSwitching = true;
+            } else if (BackSlotMain.CONFIG.put_aside) {
+                // try to put aside to an empty inventory slot if there is any
+                int slotToPutAside = playerInventory.getEmptySlot();
+                boolean canPutAside = slotToPutAside >= 0;
+                if (canPutAside) {
+                    // do switch while putting item being held aside
+                    ItemStack stackToPutAside = stackToPutBack;
+                    stackToPutBack = ItemStack.EMPTY;
+                    playerInventory.setStack(slotToPutAside, stackToPutAside); // put aside
+                    playerInventory.setStack(slotToPullOutTo, stackInSlotToPullOutFrom); // pull out
+                    playerInventory.setStack(slotToPutBackTo, stackToPutBack); // put back
+                    playerInventory.markDirty();
+                    doneSwitching = true;
+                } else if (BackSlotMain.CONFIG.drop_holding) {
+                    // even drop an item stack being held in order to make an empty space for
+                    // pulling out
+                    ItemStack stackToDrop = playerInventory.removeStack(slotToPullOutTo);
+                    ItemStack stackRemaining = playerInventory.getStack(slotToPullOutTo);
+                    // line below is from implementation of ServerPlayerEntity.dropSelectedItem()
+                    // without fully understanding what it does
+                    player.currentScreenHandler.getSlotIndex(playerInventory, slotToPullOutTo).ifPresent((i) -> {
+                        player.currentScreenHandler.setPreviousTrackedSlot(i, stackRemaining);
+                    });
+                    player.dropItem(stackToDrop, false, true);
+                    stackToPutBack = ItemStack.EMPTY;
+                    // do switch with an empty hand
+                    playerInventory.setStack(slotToPullOutTo, stackInSlotToPullOutFrom); // pull out
+                    playerInventory.setStack(slotToPutBackTo, stackToPutBack); // put back
+                    playerInventory.markDirty();
+                    doneSwitching = true;
+                }
+            }
+
+            // play sound if done switching
+            if (doneSwitching) {
+                if (BackSlotMain.CONFIG.backslot_sounds) {
+                    if (stackInSlotToPullOutTo.isEmpty() && !stackInSlotToPullOutFrom.isEmpty()) {
+                        if (stackInSlotToPullOutFrom.getItem() instanceof SwordItem) {
+                            // pulling out sword to an empty hand
+                            player.getWorld().playSound(null, player.getBlockPos(), BackSlotSounds.SHEATH_SWORD_EVENT, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                        } else {
+                            // pulling out others to an empty hand
+                            player.getWorld().playSound(null, player.getBlockPos(), SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                        }
+                    } else if (stackInSlotToPullOutFrom.getItem() instanceof SwordItem) {
+                        // pulling out sword to a non empty hand
+                        player.getWorld().playSound(null, player.getBlockPos(), BackSlotSounds.SHEATH_SWORD_EVENT, SoundCategory.PLAYERS, 1.0F, 0.9F + player.getRandom().nextFloat() * 0.2F);
+                    } else if (stackInSlotToPullOutTo.getItem() instanceof SwordItem) {
+                        // putting back sword item (including while pulling out what there was other
+                        // than sword)
+                        player.getWorld().playSound(null, player.getBlockPos(), BackSlotSounds.PACK_UP_ITEM_EVENT, SoundCategory.PLAYERS, 1.0F, 0.9F + player.getRandom().nextFloat() * 0.2F);
+                    } else if (!stackInSlotToPullOutTo.isEmpty()) {
+                        // putting back other than sword
+                        player.getWorld().playSound(null, player.getBlockPos(), SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                    }
+                }
+            }
+        });
     }
 
     public static boolean isItemAllowed(ItemStack stack, int slot) {
